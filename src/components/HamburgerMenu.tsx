c6a75dc80b8c9c4471a773fb8bb5a1160e727c98
mochisatorudev <afelicidadedasuaxota@gmail.com>
2025-08-31 18:31:53 -0300

    first commit

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -79,7 +79,7 @@ const HamburgerMenu: React.FC = () => {
               'transition-transform duration-300 ease-in-out',
               'w-4/5 max-w-xs',
               'px-5 pb-8', // padding interno
-              'rounded-bl-3xl rounded-tl-none', // apenas canto inferior esquerdo arredondado
+              '', // sem bordas arredondadas
               'shadow-xl', // sombra mais suave
               'border-l border-blue-100', // borda sutil à esquerda
               open ? 'translate-x-0' : 'translate-x-full',
